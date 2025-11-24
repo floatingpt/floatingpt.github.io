ceba187fb72ae1951ce2648a2f7c88e7b09d647b
floatingpt <jmmaestas@ucdavis.edu>
2025-11-24 04:31:55 +0000

    Deploying to gh-pages from @ floatingpt/floatingpt.github.io@2a0cc2c7f304cf6929e3ca4027740a83cb8efd29 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "floatingpt/jpm_3005.io",
+    "data-repo": "floatingpt/floatingpt.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
